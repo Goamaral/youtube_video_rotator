11add91d4ae2892a07c48da125b429792d833ac8
Goncalo Amaral <goa.docs@gmail.com>
2019-01-26 23:52:29 +0000

    Created script

--- rotator.js
+++ rotator.js
@@ -1,3 +1,28 @@
 (function() {
-  window.alert("IM HERE!")
+  if (location.host == "www.youtube.com") {
+    switch (location.pathname) {
+      case "/playlist":
+        document.querySelector("#content > a").click()
+        break
+
+      case "/watch":
+        if (location.search.includes("list")) {
+          let one_second = 1000
+          let one_minute = 60 * one_second
+          let ten_minutes = 10 * one_minute
+          let done = false
+
+          setTimeout(() => {
+            document.querySelector("#movie_player > div.ytp-chrome-bottom"
+             + " > div.ytp-chrome-controls"
+             + " > div.ytp-left-controls"
+             + " > a.ytp-next-button.ytp-button"
+            ).click()
+          }, ten_minutes)
+        } else {
+          window.alert("Go to a playlist")
+        }
+        break
+    }
+  }
 })()
